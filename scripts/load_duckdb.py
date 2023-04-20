13e9d1009c258ed001756d5319a250511aa95bce
gtg269c <aminmom@hotmail.com>
2023-04-20 12:26:20 -0700

    create duckdb table by study and test

--- scripts/load_duckdb.py
+++ scripts/load_duckdb.py
@@ -8,41 +8,45 @@ import duckdb
 # Specify the Parquet file path and the size of each chunk to read
 parquet_file = '/mnt/d/Research/Omics/Xena/data/file_tpm_pheno_gene_pqt.parquet'
 database_name = '/mnt/d/Research/Omics/Xena/data/TcgaTargetGtex_xena.duckdb'
-# query data from parquet and save to duckdb table
-# load_query = """
-#     CREATE OR REPLACE TABLE stt3a_gtex_table
-#     AS SELECT tpm, gene, _study, _primary_site
-#     FROM parquet_scan('{}')
-#     WHERE gene = 'STT3A' AND _study = 'GTEX'
-#     """.format(input_file)
 
-chunk_size = 1000
+# class to connect, access duckdb database
 
-# Connect to DuckDB in memory database
-conn = duckdb.connect(database=database_name, read_only=False)
 
-# create table in duckdb and load data from parquet
-# conn.execute("CREATE OR REPLACE TABLE gtex_table AS SELECT tpm, gene, _study, _primary_site FROM parquet_scan('{}') WHERE _study = 'GTEX'".format(parquet_file))
+class DuckDB:
+    def __init__(self, database_name):
+        self.database_name = database_name
+        self.conn = duckdb.connect(database=database_name, read_only=False)
 
-# count records in duckdb table
-df = conn.execute("SELECT COUNT(*) FROM gtex_table WHERE gene='SPTLC1'").df()
-print(f"Number of records in duckdb table: {df}")
+    @staticmethod
+    def create_table_query(file_parquet: str, studyid: str):
+        query_load_study = f"""
+                            CREATE OR REPLACE TABLE {studyid.lower()}_table
+                            AS SELECT tpm, gene, _study, _primary_site
+                            FROM parquet_scan('{file_parquet}')
+                            WHERE _study = '{studyid}'
+                            """
+        return query_load_study
 
-# conn = duckdb.connect(database=":memory:", read_only=False)
-# conn.execute(
-#     'CREATE OR REPLACE TABLE gtex_table (tpm FLOAT, gene STRING, _study STRING, _primary_site STRING, detailed_category STRING)')
+    def create_table(self, file_parquet: str, studyid: str):
+        query = DuckDB.create_table_query(file_parquet, studyid)
+        self.conn.execute(query)
 
-# # Read the Parquet file in chunks and insert each chunk into the DuckDB table
-# with pq.ParquetFile(parquet_file) as pf:
-#     for i in range(pf.num_row_groups):
-#         start = i * chunk_size
-#         end = min(start + chunk_size, pf.metadata.num_rows)
-#         table = pf.read(columns=['tpm', 'gene', '_study',
-#                         '_primary_site', 'detailed_category'], row_group=i, start=start, stop=end)
-#         rows = [tuple(row) for row in table]
-#         query = f"INSERT INTO stt3a_gtex_table VALUES {','.join(['(?,?,?,?)']*len(rows))}"
-#         conn.execute(query, rows)
+    def close(self):
+        self.conn.close()
 
-# # count the number of rows in the parquet file using duckdb
-# conn.execute(
-#     "SELECT COUNT(*) FROM parquet_scan('{}')".format(parquet_file)).fetchall()
+
+# create duckdb database and load data from parquet
+def create_database(database_name: str, parquet_file: str, studyid: str):
+    # create duckdb database table for each study
+    for study in studyid:
+        print(f"Loading {study} data into duckdb database")
+        duckdb_obj = DuckDB(database_name)
+        duckdb_obj.create_table(parquet_file, study)
+        duckdb_obj.close()
+        print(f"Finished loading {study} data into duckdb database")
+
+
+# main function
+if __name__ == "__main__":
+    list_studies = ['GTEX', 'TCGA', 'TARGET']
+    create_database(database_name, parquet_file, list_studies)
